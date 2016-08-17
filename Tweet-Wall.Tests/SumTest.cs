9c3261e0f2d008f61951abd13d3a5b017e771504
Edin Karalic <edin.karalic@myib.com>
2016-08-17 22:39:05 +0200

    Added unit test  project.

---
+++ Tweet-Wall.Tests/SumTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Tweet_Wall.Tests
+{
+    [TestFixture]
+    public class SumTest
+    {
+        [Test]
+        public void AssertThatSumOfTwoNumbersIsCorrect()
+        {
+            int actual = 3 + 6;
+
+            Assert.AreEqual( 9, actual );
+        }
+    }
+}
